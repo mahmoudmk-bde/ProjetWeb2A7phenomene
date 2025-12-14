3e910d4a11793d7c2a0dc1911447c2044037c3b4
mahmoud <Mahmoud.Mkaddem@Esrpit.tn>
2025-12-14 19:51:41 +0100

    config fix

--- db_config.php
+++ db_config.php
@@ -2,7 +2,6 @@
 if (!class_exists('config')) {
     class config
     {
-<<<<<<< HEAD
         if (!isset(self::$pdo)) {
             $servername = "localhost";
             $username = "root";
@@ -10,17 +9,6 @@ if (!class_exists('config')) {
             $dbname = "projetweb3";
             $port = 3306; // Vérifiez si c'est bien le bon port
             
-=======
-        public static function getConnexion()
-        {
-            $host = "localhost";
-            $db   = "projetweb3"; 
-            $user = "root";
-            $pass = "";
-
-            $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
-
->>>>>>> 435a8ac689491ba15e90a3bbe2ddb576d6e4b42d
             try {
                 $pdo = new PDO($dsn, $user, $pass);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
