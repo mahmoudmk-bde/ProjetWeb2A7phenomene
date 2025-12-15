05873df28183eb9ed7db2905c99361783284bbce
mahmoud <Mahmoud.Mkaddem@Esrpit.tn>
2025-12-15 10:37:19 +0100

    azdad

--- view/backoffice/events/evenement.php
+++ view/backoffice/events/evenement.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
-require_once '../../config.php';
-require_once '../../model/evenementModel.php';
+// Use project DB config; fallback path relative to this file
+if (!class_exists('config')) {
+    $dbCfg = __DIR__ . '/../../../db_config.php';
+    if (file_exists($dbCfg)) {
+        require_once $dbCfg;
+    }
+}
+require_once __DIR__ . '/../../../model/evenementModel.php';
 
 // Simuler une session d'administration si nécessaire
 if (!isset($_SESSION['user_id'])) {
